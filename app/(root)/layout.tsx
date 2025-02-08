24a40145a6ad59da52ac807356d45178573f98d4
Chatinun <Chatinun.hobby@gmail.com>
2025-02-08 11:22:28 +0900

    config nextauth

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,13 @@
+import Navbar from "@/components/Navbar";
+import React from "react";
+
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <main className="font-sans">
+      <Navbar />
+      {children}
+    </main>
+  );
+};
+
+export default Layout;
---
+++ app/(root)/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const page = () => {
+  return <h1>HOMEPAGE</h1>;
+};
+
+export default page;
