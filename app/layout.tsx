36052168353adccf93c1950c0d84ee3ce3b54e21
Chatinun <Chatinun.hobby@gmail.com>
2025-02-09 11:22:00 +0900

    Commit in the past

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
+import "easymde/dist/easymde.min.css";
 
 const workSans = localFont({
   src: [
@@ -28,9 +29,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${workSans.variable} antialiased`}>
-        {children}
-      </body>
+      <body className={`${workSans.variable} antialiased`}>{children}</body>
     </html>
   );
 }
